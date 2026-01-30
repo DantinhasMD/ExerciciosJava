dccffa33dd3d1994dc94e65d6b3d60a6c12bc91e
Maria <eduarda25.mecd@gmail.com>
2026-01-30 15:45:02 -0300

    feat: Resolvido questões 30/01/2026

--- src/java_daily_challenges/Desafio_2026_01_30_0024.java
+++ src/java_daily_challenges/Desafio_2026_01_30_0024.java
@@ -1,5 +1,9 @@
 package java_daily_challenges;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 ========================================
 Sistema de Cadastro de Alunos
@@ -8,7 +12,8 @@ Data: 2026_01_30_0024
 ========================================
 
 ENUNCIADO:
-Desenvolva um sistema que permita cadastrar alunos, armazenando seus dados em um array. O sistema deve permitir adicionar, remover e buscar alunos por nome.
+Desenvolva um sistema que permita cadastrar alunos, armazenando seus dados em um array.
+O sistema deve permitir adicionar, remover e buscar alunos por nome.
 
 REQUISITOS:
 - O sistema deve armazenar os dados dos alunos em um array
@@ -21,9 +26,122 @@ Entrada: add Aluno 1 | Saída esperada: Aluno 1 adicionado com sucesso
 Entrada: remove Aluno 1 | Saída esperada: Aluno 1 removido com sucesso
 Entrada: buscar Aluno 1 | Saída esperada: Aluno 1 encontrado
 */
+class Aluno{
+    private String nome;
+    private int idade;
+    private int anoLetivo;
+
+    public Aluno(String nome, int idade, int anoLetivo){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoLetivo = anoLetivo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public int getAnoLetivo() {
+        return anoLetivo;
+    }
+}
 
 public class Desafio_2026_01_30_0024 {
     public static void main(String[] args) {
-        // Implemente sua solução aqui
+        List<Aluno> alunos = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\nSistema de Cadastro de Alunos");
+            System.out.println("1 - Adicionar Aluno");
+            System.out.println("2 - Remover Aluno");
+            System.out.println("3 - Buscar Aluno");
+            System.out.println("4 - Sair");
+            System.out.print("Selecione a opção desejada: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao){
+                case 1:
+                    System.out.print("\nNome do Aluno: ");
+                    String nome = sc.nextLine();
+
+                    if (nome.isEmpty()){
+                        System.out.println("Coloque um nome válido!");
+                        break;
+                    }
+
+                    System.out.println("\nIdade do Aluno: ");
+                    int idade = sc.nextInt();
+                    sc.nextLine();
+
+                    if (idade > 20 || idade < 5){
+                        System.out.println("Idade inválida par ao sistema escolar!");
+                        break;
+                    }
+
+                    System.out.println("\nSérie Escolar: ");
+                    int anoLetivo = sc.nextInt();
+                    sc.nextLine();
+
+                    if (anoLetivo > 9){
+                        System.out.println("Ano escolar Inválido!");
+                        break;
+                    }
+
+                    System.out.println("Aluno adicionado com sucesso!");
+                    alunos.add(new Aluno(nome, idade, anoLetivo));
+                    break;
+
+                case 2:
+                    System.out.print("\nNome do Aluno: ");
+                    String remover = sc.nextLine();
+                    boolean removido = false;
+
+                    for (int i = 0; i < alunos.size(); i++) {
+                        if (alunos.get(i).getNome().equalsIgnoreCase(remover)) {
+                            alunos.remove(i);
+                            removido = true;
+                            System.out.println("Aluno removido com sucesso!");
+                            break;
+                        }
+                    }
+
+                    if (!removido) {
+                        System.out.println("Aluno não encontrado!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("\nNome do Aluno: ");
+                    String buscar = sc.nextLine();
+                    boolean encontrado = false;
+
+                    for (Aluno a : alunos){
+                        if (a.getNome().equalsIgnoreCase(buscar)){
+                            System.out.printf("\nAluno: %s | Idade: %d | Série escolar: %d° - ano Fundamental", a.getNome(), a.getIdade(), a.getAnoLetivo());
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Aluno não encontrado!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Encerrando o programa!");
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
+        } while(opcao != 4);
+        sc.close();
     }
 }
--- src/java_daily_challenges/Desafio_2026_01_30_0651.java
+++ src/java_daily_challenges/Desafio_2026_01_30_0651.java
@@ -1,5 +1,9 @@
 package java_daily_challenges;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 ========================================
 Sistema de Votação Simples
@@ -19,9 +23,107 @@ Entrada: votar João | Saída esperada: Candidato João recebeu 1 voto
 Entrada: votar Maria | Saída esperada: Candidato Maria recebeu 1 voto
 Entrada: exibir resultados | Saída esperada: Candidato João recebeu 2 votos, Candidato Maria recebeu 1 voto
 */
+class Candidato{
+    private String nome;
+    private int numero;
+    private int qtdVotos;
+
+    public Candidato(String nome, int numero, int qtdVotos){
+        this.nome = nome;
+        this.numero = numero;
+        this.qtdVotos = qtdVotos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getQtdVotos() {
+        return qtdVotos;
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+    protected void setQtdVotos(int qtdVotos) {
+        this.qtdVotos = qtdVotos;
+    }
+
+    public void votar() {
+        setQtdVotos(getQtdVotos() + 1);
+    }
+}
 
 public class Desafio_2026_01_30_0651 {
     public static void main(String[] args) {
-        // Implemente sua solução aqui
+        List<Candidato> candidatos = new ArrayList<>();
+        candidatos.add(new Candidato("Gil do Vigor", 3456, 0));
+        candidatos.add(new Candidato("Hytalo Santos", 8902, 0));
+        candidatos.add(new Candidato("Maria Venture", 5621, 0));
+        candidatos.add(new Candidato("Maisa Silva", 7939, 0));
+
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\nSistema de Votação Simples");
+            System.out.println("1 - Visualizar Candidatos");
+            System.out.println("2 - Votar");
+            System.out.println("3 - Encerrar votações");
+            System.out.print("\nOpção deseja: ");
+            opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Candidatos Participantes:");
+                    for (Candidato c: candidatos) {
+                        System.out.printf("\n %d - %s", c.getNumero(), c.getNome());
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("\nInforme o número do Candidato que deseja votar: ");
+                    int votar = sc.nextInt();
+                    boolean encontrado = false;
+
+                    for (Candidato c: candidatos){
+                        if (c.getNumero() == votar){
+                            c.votar();
+                            System.out.printf("\nVocê votou no Candidato - %s", c.getNome());
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado){
+                        System.out.println("Candidato não encontrado!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Votações encerradas!!");
+                    Candidato vencedor = candidatos.get(0);
+
+                    for (Candidato c : candidatos) {
+                        if (c.getQtdVotos() > vencedor.getQtdVotos()) {
+                            vencedor = c;
+                        }
+                    }
+
+                    if (vencedor.getQtdVotos() == 0 ) {
+                        System.out.println("Nenhum candidado recebeu votos");
+                        break;
+                    }
+
+                    System.out.printf("O vencedor é %s com %02d voto(s)\n", vencedor.getNome(), vencedor.getQtdVotos());
+                    System.out.println("\nEncerrando o Programa! Até Mais!");
+                    break;
+
+
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
+
+        } while (opcao != 3);
+        sc.close();
     }
 }
